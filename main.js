210845d09e138e5b2a0e3e99b5bf4abe5cd4bd33
Khin-kun <87539286+Khin-kun@users.noreply.github.com>
2021-08-27 14:11:24 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+//COMANDS
+const cl = console.log;
+const a = alert;
+const AEL = addEventListener;
+const gComCss = getComputedStyle;
+const REL = removeEventListener;
+const ce = console.error;
+const ca_frame = cancelAnimationFrame;
+const ra_frame = requestAnimationFrame;
+const tString = toString;
+//DELETE
+delete getComputedStyle,alert,console.log,addEventListener,removeEventListener,console.error,cancelAnimationFrame,requestAnimationFrame,toString;
